8d3a0e992decac03fcda828e73f318469a360355
AyushGinoya <ginoyaayush@gmail.com>
2023-09-17 18:18:59 +0530

    commit

---
+++ upertolower.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   
+   string str="ayushginoya";
+
+   //cout<<int('a')-int('A');
+
+   for(int i=0;i<str.length();i++){
+        str[i]=str[i]-32;
+   }
+    cout<<str<<endl;
+
+    string str1="AYUSHGINOYA";
+    for(int i=0;i<str1.length();i++){
+        str1[i]=str1[i]+32;
+   }
+
+   cout<<str1<<endl;
+    return 0;
+}
